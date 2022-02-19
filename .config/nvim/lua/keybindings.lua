cfe34c998d633008d224a3ce5184409e6cfc59e9
MitiSit <mitisit89@gmail.com>
2022-02-19 21:40:58 +0300

    add new keybinding

--- .config/nvim/lua/keybindings.lua
+++ .config/nvim/lua/keybindings.lua
@@ -1,16 +1,17 @@
-  local default_opt = {noremap = true, silent = true}
-  local nvim_set_keymap = vim.api.nvim_set_keymap
-vim.g.mapleader=' '
+  local opts = {noremap = true, silent = true}
+  local bind = vim.api.nvim_set_keymap
+--vim.g.bindleader='<space>'
 
 --Telescope
-nvim_set_keymap("n", "<leader>ff", ':lua require"telescope.builtin".find_files()<CR>',default_opt )
-nvim_set_keymap( "n", "<leader>sf", ':lua require"telescope.builtin".current_buffer_fuzzy_find({ previewer = false, sorting_strategy="ascending" })<CR>',default_opt )
-nvim_set_keymap("n", "<leader>sb", ':lua require"telescope.builtin".buffers()<CR>',default_opt )
+bind("n", "<leader>gt", ':Telescope lsp_definitions<CR>',opts )
+bind("n", "<leader>ff", ':Telescope find_files<CR>',opts )
+bind( "n", "<leader>sf", ':Telescope current_buffer_fuzzy_find preview=false,sorting_strategy="ascending"<CR>',opts )
+bind("n", "<leader>sb", ':Telescope buffers<CR>',opts )
 --NvimTree
-vim.cmd"nmap <space>e :NvimTreeToggle<CR>"
-vim.cmd"map <space>r :NvimTreeRefresh<CR>"
---Barbar
+bind('n', '<space>e', ':NvimTreeToggle<CR>', opts)
+bind( 'n', '<space>b', ':NvimTreeFocus<CR>', opts)
+bind('n', '<space>r', ':NvimTreeRefresh<CR>', opts)
 --<A>-alt
-nvim_set_keymap('n', '<A-,>', ':BufferPrevious<CR>', default_opt)
-nvim_set_keymap('n', '<A-.>', ':BufferNext<CR>', default_opt) 
-nvim_set_keymap('n', '<C-c>', ':BufferClose<CR>',default_opt)
+bind('n', '<A-,>', ':BufferPrevious<CR>', opts)
+bind('n', '<A-.>', ':BufferNext<CR>', opts) 
+bind('n', '<C-c>', ':BufferClose<CR>',opts)
