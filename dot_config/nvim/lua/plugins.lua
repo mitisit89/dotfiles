0d157266ab51cf4556df0b88f01c14c906edfe43
mitisit <mitisit89@gmail.com>
2023-01-26 18:59:34 +0300

    Update .config/nvim/lua/plugins.lua

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -1,36 +1,44 @@
+return require("packer").startup(function(use)
+	-- Packer can manage itself
+	use({
+		"wbthomason/packer.nvim",
+		{
+			"nvim-tree/nvim-tree.lua",
+			tag = "nightly",
+		},
+		{
+			"nvim-telescope/telescope.nvim",
+			tag = "0.1.x",
+		},
+		"nvim-lualine/lualine.nvim",
+		"jose-elias-alvarez/null-ls.nvim",
+		"nvim-tree/nvim-web-devicons",
+		"romgrk/barbar.nvim",
+		"Shatur/neovim-ayu",
+		"nvim-treesitter/nvim-treesitter",
+		"lewis6991/gitsigns.nvim",
+		"nvim-lua/plenary.nvim",
+		{
+			"VonHeikemen/lsp-zero.nvim",
+			branch = "v1.x",
+			requires = {
+				-- LSP Support
+				{ "neovim/nvim-lspconfig" }, -- Required
+				{ "williamboman/mason.nvim" }, -- Optional
+				{ "williamboman/mason-lspconfig.nvim" }, -- Optional
 
-return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use{ 'wbthomason/packer.nvim',
-  {
-  'nvim-telescope/telescope.nvim', tag = '0.1.x'},
-  'nvim-lualine/lualine.nvim',
-  'jose-elias-alvarez/null-ls.nvim',
-  'nvim-tree/nvim-web-devicons',
-   'romgrk/barbar.nvim',
-  'Shatur/neovim-ayu',
-  'nvim-treesitter/nvim-treesitter',
-	'lewis6991/gitsigns.nvim' ,'nvim-lua/plenary.nvim',
-	{'VonHeikemen/lsp-zero.nvim',
-  branch = 'v1.x',
-  requires = {
-    -- LSP Support
-    {'neovim/nvim-lspconfig'},             -- Required
-    {'williamboman/mason.nvim'},           -- Optional
-    {'williamboman/mason-lspconfig.nvim'}, -- Optional
+				-- Autocompletion
+				{ "hrsh7th/nvim-cmp" }, -- Required
+				{ "hrsh7th/cmp-nvim-lsp" }, -- Required
+				{ "hrsh7th/cmp-buffer" }, -- Optional
+				{ "hrsh7th/cmp-path" }, -- Optional
+				{ "saadparwaiz1/cmp_luasnip" }, -- Optional
+				{ "hrsh7th/cmp-nvim-lua" }, -- Optional
 
-    -- Autocompletion
-    {'hrsh7th/nvim-cmp'},         -- Required
-    {'hrsh7th/cmp-nvim-lsp'},     -- Required
-    {'hrsh7th/cmp-buffer'},       -- Optional
-    {'hrsh7th/cmp-path'},         -- Optional
-    {'saadparwaiz1/cmp_luasnip'}, -- Optional
-    {'hrsh7th/cmp-nvim-lua'},     -- Optional
-
-    -- Snippets
-    {'L3MON4D3/LuaSnip'},             -- Required
-    {'rafamadriz/friendly-snippets'}, -- Optional
-  }}
-  
-}
+				-- Snippets
+				{ "L3MON4D3/LuaSnip" }, -- Required
+				{ "rafamadriz/friendly-snippets" }, -- Optional
+			},
+		},
+	})
 end)
