82aa280236992b242df5190ee1c417ea07bf70b6
mitisit <mitisit89@gmail.com>
2023-04-19 09:56:04 +0300

    Update .config/nvim/lua/general.lua

--- dot_config/nvim/lua/general.lua
+++ dot_config/nvim/lua/general.lua
@@ -1,81 +1,81 @@
 local opts = {
-	backup = false, -- creates a backup file
-	clipboard = "unnamedplus", -- allows neovim to access the system clipboard
-	cmdheight = 2, -- more space in the neovim command line for displaying messages
-	colorcolumn = "99999", -- fixes indentline for now
-	completeopt = { "menuone", "noselect" },
-	conceallevel = 0, -- so that `` is visible in markdown files
-	fileencoding = "utf-8", -- the encoding written to a file
-	foldmethod = "manual", -- folding set to "expr" for treesitter based folding
-	foldexpr = "", -- set to "nvim_treesitter#foldexpr()" for treesitter based folding
-	hidden = true, -- required to keep multiple buffers and open multiple buffers
-	hlsearch = true, -- highlight all matches on previous search pattern
-	ignorecase = true, -- ignore case in search patterns
-	mouse = "a", -- allow the mouse to be used in neovim
-	pumheight = 10, -- pop up menu height
-	showmode = false, -- we don't need to see things like -- INSERT -- anymore
-	showtabline = 2, -- always show tabs
-	smartcase = true, -- smart case
-	smartindent = true, -- make indenting smarter again
-	splitbelow = true, -- force all horizontal splits to go below current window
-	autoindent = true,
-	splitright = true, -- force all vertical splits to go to the right of current window
-	swapfile = false, -- creates a swapfile
-	termguicolors = true, -- set term gui colors (most terminals support this)
-	undodir = vim.fn.stdpath("cache") .. "/undo",
-	undofile = true, -- enable persistent undo
-	updatetime = 300, -- faster completion
-	writebackup = false, -- if a file is being edited by another program (or was written to file while editing with another program) it is not allowed to be edited
-	expandtab = true, -- convert tabs to spaces
-	shiftwidth = 2, -- the number of spaces inserted for each indentation
-	tabstop = 2, -- insert 2 spaces for a tab
-	cursorline = true, -- highlight the current line
-	number = true, -- set numbered lines
-	numberwidth = 4, -- set number column width to 2 {default 4}
-	signcolumn = "yes", -- always show the sign column otherwise it would shift the text each time
-	wrap = false, -- display lines as one long line
-	spell = true,
-	spelllang = "en",
-	scrolloff = 8, -- is one of my fav
-	sidescrolloff = 8,
-	timeoutlen = 550, -- time to wait for a mapped sequence to complete (in milliseconds) useful for mech keyboard
-	langmap = "ФИСВУАПРШОЛДЬТЩЗЙКЫЕГМЦЧНЯ;ABCDEFGHIJKLMNOPQRSTUVWXYZ,фисвуапршолдьтщзйкыегмцчня;abcdefghijklmnopqrstuvwxyz",
+  backup = false,            -- creates a backup file
+  clipboard = "unnamedplus", -- allows neovim to access the system clipboard
+  cmdheight = 2,             -- more space in the neovim command line for displaying messages
+  colorcolumn = "99999",     -- fixes indentline for now
+  completeopt = { "menuone", "noselect" },
+  conceallevel = 0,          -- so that `` is visible in markdown files
+  fileencoding = "utf-8",    -- the encoding written to a file
+  foldmethod = "manual",     -- folding set to "expr" for treesitter based folding
+  foldexpr = "",             -- set to "nvim_treesitter#foldexpr()" for treesitter based folding
+  hidden = true,             -- required to keep multiple buffers and open multiple buffers
+  hlsearch = true,           -- highlight all matches on previous search pattern
+  ignorecase = true,         -- ignore case in search patterns
+  mouse = "a",               -- allow the mouse to be used in neovim
+  pumheight = 10,            -- pop up menu height
+  showmode = false,          -- we don't need to see things like -- INSERT -- anymore
+  showtabline = 2,           -- always show tabs
+  smartcase = true,          -- smart case
+  smartindent = true,        -- make indenting smarter again
+  splitbelow = true,         -- force all horizontal splits to go below current window
+  autoindent = true,
+  splitright = true,         -- force all vertical splits to go to the right of current window
+  swapfile = false,          -- creates a swapfile
+  termguicolors = true,      -- set term gui colors (most terminals support this)
+  undodir = vim.fn.stdpath("cache") .. "/undo",
+  undofile = true,           -- enable persistent undo
+  updatetime = 300,          -- faster completion
+  writebackup = false,       -- if a file is being edited by another program (or was written to file while editing with another program) it is not allowed to be edited
+  expandtab = true,          -- convert tabs to spaces
+  shiftwidth = 2,            -- the number of spaces inserted for each indentation
+  tabstop = 2,               -- insert 2 spaces for a tab
+  cursorline = true,         -- highlight the current line
+  number = true,             -- set numbered lines
+  numberwidth = 4,           -- set number column width to 2 {default 4}
+  signcolumn = "yes",        -- always show the sign column otherwise it would shift the text each time
+  wrap = false,              -- display lines as one long line
+  spell = true,
+  spelllang = "en",
+  scrolloff = 8,    -- is one of my fav
+  sidescrolloff = 8,
+  timeoutlen = 850, -- time to wait for a mapped sequence to complete (in milliseconds) useful for mech keyboard
+  langmap = "ФИСВУАПРШОЛДЬТЩЗЙКЫЕГМЦЧНЯ;ABCDEFGHIJKLMNOPQRSTUVWXYZ,фисвуапршолдьтщзйкыегмцчня;abcdefghijklmnopqrstuvwxyz",
 }
 
 for key, value in pairs(opts) do
-	vim.opt[key] = value
+  vim.opt[key] = value
 end
 vim.api.nvim_create_autocmd("InsertEnter", {
-	callback = function()
-		vim.opt.relativenumber = false
-	end,
+  callback = function()
+    vim.opt.relativenumber = true
+  end,
 })
 vim.api.nvim_create_autocmd("InsertLeave", {
-	callback = function()
-		vim.opt.relativenumber = true
-	end,
+  callback = function()
+    vim.opt.relativenumber = false
+  end,
 })
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
 vim.diagnostic.config({
-	virtual_text = false,
-	signs = true,
-	underline = true,
-	update_in_insert = false,
-	severity_sort = false,
+  virtual_text = false,
+  signs = true,
+  underline = true,
+  update_in_insert = false,
+  severity_sort = false,
 })
 vim.api.nvim_create_autocmd("CursorHold", {
-	buffer = bufnr,
-	callback = function()
-		local opt = {
-			focusable = false,
-			close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
-			border = "rounded",
-			source = "always",
-			prefix = " ",
-			scope = "line",
-		}
-		vim.diagnostic.open_float(nil, opt)
-	end,
+  buffer = bufnr,
+  callback = function()
+    local opt = {
+      focusable = false,
+      close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
+      border = "rounded",
+      source = "always",
+      prefix = " ",
+      scope = "line",
+    }
+    vim.diagnostic.open_float(nil, opt)
+  end,
 })
