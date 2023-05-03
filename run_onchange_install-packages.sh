b501f1efd2fa3595f53c4adc9ee32a4f47ab932e
mitisit <mitisit89@gmail.com>
2023-05-04 00:48:10 +0300

    Update .config/chezmoi/chezmoistate.boltdb Add install-packages.sh

---
+++ run_onchange_install-packages.sh
@@ -0,0 +1,15 @@
+{{ if eq .chezmoi.os "linux" -}} 
+#!/bin/sh
+mkdir ~/.local/state/{bash,zsh} 
+mkdir ~/.local/bin/
+{{ end -}}
+{{ if eq .chezmoi.osRelease.id "pop" -}}
+#!/bin/sh
+sudo apt install -y ripgrep fzf curl
+ 
+NVIM_URL="https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.tar.gz"
+NVIM_DEST="$HOME/.local/bin/nvim"
+curl -L $NVIM_URL | tar xz --strip-components=1 -C $NVIM_DEST
+chmod u+x $NVIM_DEST/nvim
+{{end}}
+
