e032b9cd72ee1b4349a1c53866361b8fad06c7a9
mitisit <mitisit89@gmail.com>
2023-06-25 12:07:41 +0300

    Update .config/alacritty/alacritty.yml Update install-packages.sh

--- run_onchange_install-packages.sh
+++ run_onchange_install-packages.sh
@@ -1,14 +1,5 @@
-{{ if eq .chezmoi.os "linux" -}} 
 #!/bin/sh
 mkdir ~/.local/state/{bash,zsh} 
 mkdir ~/.local/bin/
-{{ else if eq .chezmoi.osRelease.id "pop" -}}
-#!/bin/sh
-sudo apt install -y ripgrep fzf curl
- 
-NVIM_URL="https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.tar.gz"
-NVIM_DEST="$HOME/.local/bin/nvim"
-curl -L $NVIM_URL | tar xz --strip-components=1 -C $NVIM_DEST
-chmod u+x $NVIM_DEST/nvim
-{{end}}
+sudo apt install ripgrep nala  fzf curl
 
