2a22fecec238724c4ca40b9a78164a605a23185f
mitisit <29955395+mitisit89@users.noreply.github.com>
2023-11-20 23:51:25 +0300

    Update .config/nvim/lazy-lock.json Update .config/nvim/lua/keymaps.lua

--- dot_config/nvim/lua/keymaps.lua
+++ dot_config/nvim/lua/keymaps.lua
@@ -11,9 +11,10 @@ map("n", "<C-b>", ":Neotree focus<CR>", opts)
 map("n", "<Leader>ff", builtin.git_files, { desc = "git_files" }, opts)
 map("n", "<leader>gf", builtin.live_grep, { desc = "live_grep" }, opts)
 map("n", "<leader>fb", builtin.buffers, { desc = "find_buffers" }, opts)
-map("n", "<leader>fs", builtin.lsp_document_symbols, { desc = "lsp_document_symbols" }, opts)
-
-
+map("n", "<leader>fs", builtin.current_buffer_fuzzy_find, { desc = "current_buffer_fuzzy_find" }, opts)
+map("n", "<leader>fb", builtin.buffers, { desc = "find_buffers" }, opts)
+map("n", "<leader>ld", builtin.lsp_definitions, { desc = "lsp_definitions" }, opts)
+map("n", "<leader>lr", builtin.lsp_references, { desc = "find_buffers" }, opts)
 
 map("n", "<leader>rn", ":lua vim.lsp.buf.rename()<CR>", opts)
 map("v", "<", "<gv", opts)
