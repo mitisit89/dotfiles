8f860453e5da4fc603151450eff9e3abf6f88bad
mitisit <29955395+mitisit89@users.noreply.github.com>
2024-02-01 19:16:47 +0300

    Update .config/nvim/lua/keymaps.lua Add .config/nvim/lua/plugins/neotest.lua

---
+++ dot_config/nvim/lua/plugins/neotest.lua
@@ -0,0 +1,17 @@
+return {
+	"nvim-neotest/neotest",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"antoinemadec/FixCursorHold.nvim",
+		"nvim-treesitter/nvim-treesitter",
+	},
+	config = function()
+		require("neotest").setup({
+			adapters = {
+				require("neotest-python")({
+					dap = { justMyCode = false },
+				}),
+			},
+		})
+	end,
+}
