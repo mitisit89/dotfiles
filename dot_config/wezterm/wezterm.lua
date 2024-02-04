0fbdfb166586928cdc890d3a3a1c2adc64e9dd20
mitisit <29955395+mitisit89@users.noreply.github.com>
2024-02-04 21:22:53 +0300

    Add .config/wezterm/wezterm.lua

---
+++ dot_config/wezterm/wezterm.lua
@@ -0,0 +1,10 @@
+local wezterm = require("wezterm")
+
+return {
+	font = wezterm.font("Hack Nerd Font"),
+	font_size = 14,
+	color_scheme = "Tokyo Night",
+	use_fancy_tab_bar = false,
+	freetype_load_target = "HorizontalLcd",
+	harfbuzz_features = { "calt=0", "clig=0", "liga=0" },
+}
