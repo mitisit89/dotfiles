1a8d27bdf813cb4d3bbd0976af30475337fd5cea
mitisit <29955395+mitisit89@users.noreply.github.com>
2024-03-16 01:04:48 +0300

    Update .config/nvim/lazy-lock.json Add .config/nvim/lua/plugins/lint.lua

---
+++ dot_config/nvim/lua/plugins/lint.lua
@@ -0,0 +1,22 @@
+return {
+	"mfussenegger/nvim-lint",
+	dependencies = {
+		"neovim/nvim-lspconfig",
+		"nvim-lua/plenary.nvim",
+	},
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		local lint = require("lint")
+		lint.linters_by_ft = {
+			python = { "ruff" },
+		}
+		local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+
+		vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+			group = lint_augroup,
+			callback = function()
+				lint.try_lint()
+			end,
+		})
+	end,
+}
