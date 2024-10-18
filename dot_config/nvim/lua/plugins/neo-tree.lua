22fd1d83e4e2205fdc77948b421e1361d22a0e55
mitisit <29955395+mitisit89@users.noreply.github.com>
2024-10-19 01:19:58 +0300

    Update .config/nvim/lua/plugins/neo-tree.lua

--- dot_config/nvim/lua/plugins/neo-tree.lua
+++ dot_config/nvim/lua/plugins/neo-tree.lua
@@ -5,7 +5,27 @@ return {
 		"nvim-lua/plenary.nvim",
 		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
 		"MunifTanjim/nui.nvim",
+		{
+			"s1n7ax/nvim-window-picker",
+			version = "2.*",
+			config = function()
+				require("window-picker").setup({
+					filter_rules = {
+						include_current_win = false,
+						autoselect_one = true,
+						-- filter using buffer options
+						bo = {
+							-- if the file type is one of following, the window will be ignored
+							filetype = { "neo-tree", "neo-tree-popup", "notify" },
+							-- if the buffer type is one of following, the window will be ignored
+							buftype = { "terminal", "quickfix" },
+						},
+					},
+				})
+			end,
+		},
 	},
+
 	config = function()
 		require("neo-tree").setup({
 			enable_git_status = true,
