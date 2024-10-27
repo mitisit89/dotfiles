c18171742b5065a1d2cc6d7cc41acddf066c8f5c
mitisit <29955395+mitisit89@users.noreply.github.com>
2024-10-27 16:00:22 +0300

    Add .config/nvim/lua/plugins/dap.lua

---
+++ dot_config/nvim/lua/plugins/dap.lua
@@ -0,0 +1,80 @@
+return {
+	{
+		"mfussenegger/nvim-dap",
+		dependencies = {
+			"leoluz/nvim-dap-go",
+			"mfussenegger/nvim-dap-python",
+			"rcarriga/nvim-dap-ui",
+			"theHamsta/nvim-dap-virtual-text",
+			"nvim-neotest/nvim-nio",
+		},
+		config = function()
+			local dap = require("dap")
+			local ui = require("dapui")
+
+			require("dapui").setup()
+			require("dap-go").setup()
+			require("dap-python").setup(vim.g.python_host_prog)
+
+			require("nvim-dap-virtual-text").setup({
+				-- This just tries to mitigate the chance that I leak tokens here. Probably won't stop it from happening...
+				display_callback = function(variable)
+					local name = string.lower(variable.name)
+					local value = string.lower(variable.value)
+					if name:match("secret") or name:match("api") or value:match("secret") or value:match("api") then
+						return "*****"
+					end
+
+					if #variable.value > 15 then
+						return " " .. string.sub(variable.value, 1, 15) .. "... "
+					end
+
+					return " " .. variable.value
+				end,
+			})
+			-- install only via pip
+			dap.adapters.python = {
+				type = "executable",
+				command = "python3",
+				args = { "-m", "debugpy.adapter" },
+			}
+			-- Handled by nvim-dap-go
+			dap.adapters.go = {
+				type = "server",
+				port = "${port}",
+				executable = {
+					command = "dlv",
+					args = { "dap", "-l", "127.0.0.1:${port}" },
+				},
+			}
+
+			vim.keymap.set("n", "<space>b", dap.toggle_breakpoint)
+			vim.keymap.set("n", "<space>gb", dap.run_to_cursor)
+
+			-- Eval var under cursor
+			vim.keymap.set("n", "<space>?", function()
+				require("dapui").eval(nil, { enter = true })
+			end)
+
+			vim.keymap.set("n", "<F3>", dap.continue)
+			vim.keymap.set("n", "<F4>", dap.step_into)
+			vim.keymap.set("n", "<F5>", dap.step_over)
+			vim.keymap.set("n", "<F6>", dap.step_out)
+			vim.keymap.set("n", "<F7>", dap.step_back)
+			vim.keymap.set("n", "<F8>", dap.restart)
+
+			dap.listeners.before.attach.dapui_config = function()
+				ui.open()
+			end
+			dap.listeners.before.launch.dapui_config = function()
+				ui.open()
+			end
+			dap.listeners.before.event_terminated.dapui_config = function()
+				ui.close()
+			end
+			dap.listeners.before.event_exited.dapui_config = function()
+				ui.close()
+			end
+		end,
+	},
+}
