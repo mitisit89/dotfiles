ec732b89a69bc6cb6125cad21d34e064acde9f97
mitisit <29955395+mitisit89@users.noreply.github.com>
2024-12-11 07:03:06 +0300

    Update .config/nvim/lua/keymaps.lua Add .config/nvim/lua/plugins/dabbod.lua Update .config/nvim/lua/plugins/lsp.lua

--- dot_config/nvim/lua/plugins/lsp.lua
+++ dot_config/nvim/lua/plugins/lsp.lua
@@ -8,6 +8,7 @@ return {
 	config = function()
 		local capabilities = vim.lsp.protocol.make_client_capabilities()
 		capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
+
 		local servers = {
 
 			lua_ls = {
@@ -32,6 +33,11 @@ return {
 				filetypes = { "sh", "zsh", "bash" },
 			},
 			gopls = {
+
+				init_options = {
+
+					usePlaceholders = true,
+				},
 				buildFlags = { "-tags=integration" },
 				experimentalPostfixCompletions = true,
 				analyses = {
