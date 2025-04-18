87df5155b6b8410240e17db1475b0865c71b5ef8
mitisit <29955395+mitisit89@users.noreply.github.com>
2025-04-19 00:14:30 +0300

    Update .config/nvim/lua/plugins/cmp.lua Update .config/nvim/lua/plugins/lsp.lua

--- dot_config/nvim/lua/plugins/cmp.lua
+++ dot_config/nvim/lua/plugins/cmp.lua
@@ -25,110 +25,56 @@ local symbol_map = {
 	Operator = "󰆕",
 	TypeParameter = "",
 }
-local helo = "world"
 return {
-	"hrsh7th/nvim-cmp",
-	version = false, -- last release is way too old
-	event = "InsertEnter",
-	dependencies = {
-		"hrsh7th/cmp-nvim-lsp",
-		"hrsh7th/cmp-nvim-lua",
-		"hrsh7th/cmp-buffer",
-		"FelipeLema/cmp-async-path",
-		"hrsh7th/cmp-cmdline",
-		"saadparwaiz1/cmp_luasnip",
-		"L3MON4D3/LuaSnip",
-		"rafamadriz/friendly-snippets",
-		"onsails/lspkind.nvim",
+	"saghen/blink.cmp",
+	-- optional: provides snippets for the snippet source
+	dependencies = { "rafamadriz/friendly-snippets" },
+
+	-- use a release tag to download pre-built binaries
+	version = "1.*",
+	-- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
+	-- build = 'cargo build --release',
+	-- If you use nix, you can build from source using latest nightly rust with:
+	-- build = 'nix run .#build-plugin',
+
+	---@module 'blink.cmp'
+	---@type blink.cmp.Config
+	opts = {
+		-- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
+		-- 'super-tab' for mappings similar to vscode (tab to accept)
+		-- 'enter' for enter to accept
+		-- 'none' for no mappings
+		--
+		-- All presets have the following mappings:
+		-- C-space: Open menu or open docs if already open
+		-- C-n/C-p or Up/Down: Select next/previous item
+		-- C-e: Hide menu
+		-- C-k: Toggle signature help (if signature.enabled = true)
+		--
+		-- See :h blink-cmp-config-keymap for defining your own keymap
+		keymap = { preset = "super-tab" },
+
+		appearance = {
+			-- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+			-- Adjusts spacing to ensure icons are aligned
+			nerd_font_variant = "mono",
+		},
+
+		-- (Default) Only show the documentation popup when manually triggered
+		completion = { documentation = { auto_show = false } },
+
+		-- Default list of enabled providers defined so that you can extend it
+		-- elsewhere in your config, without redefining it, due to `opts_extend`
+		sources = {
+			default = { "lsp", "path", "snippets", "buffer" },
+		},
+
+		-- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
+		-- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
+		-- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
+		--
+		-- See the fuzzy documentation for more information
+		fuzzy = { implementation = "prefer_rust_with_warning" },
 	},
-
-	opts = function()
-		local luasnip = require("luasnip")
-		local cmp = require("cmp")
-		local defaults = require("cmp.config.default")()
-
-		local cmp_ui = {
-			icons = true,
-			lspkind_text = true,
-			style = "default", -- default/flat_light/flat_dark/atom/atom_colored
-			border_color = "grey_fg", -- only applicable for "default" style, use color names from base30 variables
-			selected_item_bg = "colored", -- colored / simple
-		}
-		local cmp_style = cmp_ui.style
-		local field_arrangement = {
-			atom = { "kind", "abbr", "menu" },
-			atom_colored = { "kind", "abbr", "menu" },
-		}
-
-		cmp.setup({
-			snippet = {
-				expand = function(args)
-					require("luasnip").lsp_expand(args.body)
-				end,
-			},
-
-			formatting = {
-				-- default fields order i.e completion word + item.kind + item.kind icons
-				fields = field_arrangement[cmp_style] or { "abbr", "kind", "menu" },
-
-				format = function(_, item)
-					local icons = symbol_map
-					local icon = (cmp_ui.icons and icons[item.kind]) or ""
-
-					if cmp_style == "atom" or cmp_style == "atom_colored" then
-						icon = " " .. icon .. " "
-						item.menu = cmp_ui.lspkind_text and "   (" .. item.kind .. ")" or ""
-						item.kind = icon
-					else
-						icon = cmp_ui.lspkind_text and (" " .. icon .. " ") or icon
-						item.kind = string.format("%s %s", icon, cmp_ui.lspkind_text and item.kind or "")
-					end
-
-					return item
-				end,
-			},
-			sorting = defaults.sorting,
-			window = {
-				completion = cmp.config.window.bordered(),
-				documentation = cmp.config.window.bordered(),
-			},
-
-			mapping = {
-				["<C-p>"] = cmp.mapping.select_prev_item(),
-				["<C-n>"] = cmp.mapping.select_next_item(),
-				["<C-d>"] = cmp.mapping.scroll_docs(-4),
-				["<C-f>"] = cmp.mapping.scroll_docs(4),
-				["<C-Space>"] = cmp.mapping.complete(),
-				["<C-e>"] = cmp.mapping.close(),
-				["<CR>"] = cmp.mapping.confirm({
-					behavior = cmp.ConfirmBehavior.Replace,
-					select = true,
-				}),
-				["<Tab>"] = function(fallback)
-					if cmp.visible() then
-						cmp.select_next_item()
-					elseif luasnip.expand_or_jumpable() then
-						luasnip.expand_or_jump()
-					else
-						fallback()
-					end
-				end,
-				["<S-Tab>"] = function(fallback)
-					if cmp.visible() then
-						cmp.select_prev_item()
-					elseif luasnip.jumpable(-1) then
-						luasnip.jump(-1)
-					else
-						fallback()
-					end
-				end,
-			},
-			sources = {
-				{ name = "nvim_lsp" },
-				{ name = "luasnip" },
-				{ name = "async_path" },
-				{ name = "buffer" },
-			},
-		})
-	end,
+	opts_extend = { "sources.default" },
 }
--- dot_config/nvim/lua/plugins/lsp.lua
+++ dot_config/nvim/lua/plugins/lsp.lua
@@ -7,7 +7,7 @@ return {
 	},
 	config = function()
 		local capabilities = vim.lsp.protocol.make_client_capabilities()
-		capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
+		capabilities = vim.tbl_deep_extend("force", capabilities, require('blink.cmp').get_lsp_capabilities())
 
 		local servers = {
 
