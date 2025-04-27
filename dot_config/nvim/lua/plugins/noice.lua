32ca4669fc0ee8abb6330ecfa674f01b614579b8
mitisit <29955395+mitisit89@users.noreply.github.com>
2025-04-28 02:27:03 +0300

    Update .config/nvim/lua/plugins/noice.lua

--- dot_config/nvim/lua/plugins/noice.lua
+++ dot_config/nvim/lua/plugins/noice.lua
@@ -10,35 +10,5 @@ return {
 		--   If not available, we use `mini` as the fallback
 		"rcarriga/nvim-notify",
 	},
-	opts = {
-		lsp = {
-			override = {
-				["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-				["vim.lsp.util.stylize_markdown"] = true,
-				["cmp.entry.get_documentation"] = true,
-			},
-			signature = {
-				enabled = false,
-			},
-		},
-		routes = {
-			{
-				filter = {
-					event = "msg_show",
-					any = {
-						{ find = "%d+L, %d+B" },
-						{ find = "; after #%d+" },
-						{ find = "; before #%d+" },
-					},
-				},
-				view = "mini",
-			},
-		},
-		presets = {
-			bottom_search = true,
-			command_palette = true,
-			long_message_to_split = true,
-			inc_rename = true,
-		},
-	},
+	opts = {},
 }
